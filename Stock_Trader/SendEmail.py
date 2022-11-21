c9561ea9a7db7163f8399fb4a8d57ece543475c1
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-21 10:19:21 +0700

    SiGG-Stock_Portfolio_Treader-SendEmail-py

---
+++ Stock_Trader/SendEmail.py
@@ -0,0 +1,69 @@
+##
+ # @file SendEmail.py
+ # 
+ # @author SiGG (Project::Stock_Portfolio_Maker_Gradient_Descent)
+ # @brief 
+ # @version 0.1
+ # @date  @date 2022-11-21
+ # 
+ # @copyright Copyright (c) 2022
+ # 
+##
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import sys
+
+error = False
+attach = False
+subject = ""
+
+#Prints usage if no params passed
+if (len(sys.argv) == 1):
+    print("Usage: SendEmail.py <-option [-e error, -a attachment]> 'Subject'")
+    exit(1)
+elif (len(sys.argv) == 2):
+    subject = "No Subject"
+else:
+    subject = str(sys.argv[2:][0])
+    
+#Error in code somewhere
+if (sys.argv[1:][0] == "-e"):
+    subject = "Error: " + subject
+    error = True
+elif (sys.argv[1:][0] == "-a"):
+    attach = True
+    
+fromaddr = "portfoliomanagerupdate@gmail.com"
+toaddr = "Eyoung8@wisc.edu"
+ 
+msg = MIMEMultipart()
+ 
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = subject
+
+body = ""
+
+#Attaches only if attachment specified
+if (attach):
+    msg.attach(MIMEText(body, 'plain'))
+         
+    filename = "CurrentPicks.txt"
+    attachment = open("./CurrentPicks.txt", "rb")
+         
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+         
+    msg.attach(part)
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "PortfolioManager")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
