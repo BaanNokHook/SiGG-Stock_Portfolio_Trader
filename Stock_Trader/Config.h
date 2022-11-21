5e2e9bce076a2525c602bbf4b829297c9fefacd7
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-21 08:51:39 +0700

    SiGG-Stock_Portfolio_Treader-Config-hpp

---
+++ Stock_Trader/Config.h
@@ -0,0 +1,23 @@
+/**
+ * @file Config.h
+ * 
+ * @author SiGG (Project::Stock_Portfolio_Maker_Gradient_Descent)
+ * @brief 
+ * @version 0.1
+ * @date  @date 2022-11-21
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+**/
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+class Config {
+public: 
+      Config();
+      virtaul ~Config();  
+};
+
+#endif /* CONFIG_H_ */
+
